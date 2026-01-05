912fddd08d601bd0a7ea0427ae5284261ab96a0e
npratik0 <neupanepratik00@gmail.com>
2026-01-05 20:37:32 +0545

    Updates

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,8 +5,6 @@ export const PORT: number =
     process.env.PORT ? parseInt(process.env.PORT) : 3000;
 export const MONGODB_URI: string =
     process.env.MONGODB_URI || 'mongodb://localhost:27017/defaultdb';
-// Application level constants, with fallbacks
-// if .env variables are not set
 
 export const JWT_SECRET: string =
     process.env.JWT_SECRET || 'default';
