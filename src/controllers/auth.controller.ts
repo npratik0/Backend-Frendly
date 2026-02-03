dcab07e5ad45b760ab394f8e80db944bc6493d23
npratik0 <neupanepratik00@gmail.com>
2026-02-03 21:31:18 +0545

    Controller updates

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import { UserService } from "../services/user.service";
 import { Request, Response } from "express";
 import z, { success } from "zod";
-import { CreateUserDTO, LoginUserDTO } from "../dtos/user.dto";
+import { CreateUserDTO, LoginUserDTO, UpdateUserDTO } from "../dtos/user.dto";
 import { parse } from "path";
 
 let userService = new UserService();
@@ -23,6 +23,7 @@ export class AuthController{
             return res.status(201).json({
                 success: true, message: "User Created", data: newUser
             });
+            console.log(req.body);
             
         }catch(error: Error | any){ // exception handling
             return res.status(500).json(
@@ -47,6 +48,54 @@ export class AuthController{
             return res.status(error.statusCode ?? 500).json(
                 {success: false, message: error.message || "Itenal Server Error"}
             )
+        }
+    }
+    
+    // Sprint 5 additional
+    async getProfile(req: Request, res: Response) {
+        try {
+            const userId = req.user?._id;
+            if (!userId) {
+                return res.status(400).json(
+                    { success: false, message: "User Id Not found" }
+                );
+            }
+            const user = await userService.getUserById(userId);
+            return res.status(200).json(
+                { success: true, data: user, message: "User profile fetched successfully" }
+            );
+        } catch (error: Error | any) {
+            return res.status(error.statusCode || 500).json(
+                { success: false, message: error.message || "Internal Server Error" }
+            );
+        }
+    }
+
+    async updateProfile(req: Request, res: Response) {
+        try {
+            const userId = req.user?._id;
+            if (!userId) {
+                return res.status(400).json(
+                    { success: false, message: "User Id Not found" }
+                );
+            }
+            const parsedData = UpdateUserDTO.safeParse(req.body);
+            if (!parsedData.success) {
+                return res.status(400).json(
+                    { success: false, message: z.prettifyError(parsedData.error) }
+                ); // z.prettifyError - better error messages (zod)
+            }
+            if (req.file) {
+                parsedData.data.imageUrl = `/uploads/${req.file.filename}`;
+            }
+            const updatedUser = await userService.updateUser(userId, parsedData.data);
+            return res.status(200).json(
+                { success: true, data: updatedUser, message: "User profile updated successfully" }
+            );
+        } catch (error: Error | any) {
+            return res.status(error.statusCode || 500).json(
+                { success: false, message: error.message || "Internal Server Error" }
+            );
         }
     }
 }
